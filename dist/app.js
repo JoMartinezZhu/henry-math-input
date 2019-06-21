7d529b949bc21e7abc890b65b3b8c3d1e64b01e7
henry <henry.zhushangjie@qq.com>
2019-06-21 17:13:25 +0800

    2.0.1-beta.2

--- dist/app.js
+++ dist/app.js
@@ -1,21 +1,23 @@
 'use strict';
 
-var React = require('react');
-var ReactDOM = require('react-dom');
+// const React = require('react');
+// const ReactDOM = require('react-dom');
 
 require('../mathquill/mathquill');
 
-var MathInput = require('./components/app');
+// const MathInput = require('./components/app');
 
-var inputlist = document.querySelectorAll("div.math-input");
+// var inputlist = document.querySelectorAll("div.math-input");
 
-for (var index = 0; index < inputlist.length; index++) {
-    var myid = inputlist[index].id;
-    ReactDOM.render(React.createElement(MathInput, { containerid: myid, focusCallback: function focusCallback() {
-            console.log('on focus');
-        }, blurCallback: function blurCallback() {
-            console.log('did dismiss');
-        }, extraKeys: ['CUP', 'CAP', 'INFTY', 'HALFCOORDINATE', 'HALFCLOSURE', 'COORDINATE', 'CLOSURE', 'DELTA', 'BIGTRIANGLEUP', 'HUO', 'SIM', 'CONG', 'PERP', 'QIE', 'APPROX', 'TRIANGLE', 'BECAUSE', 'THEREFORE', 'ODOT', 'MAO', 'ANGLE', 'JIAO', 'PERCENT', 'DU', 'DEGREE', 'GAMMA', 'EPSILON', 'VAREPSILON', 'ZETA', 'ETA', 'VARTHETA', 'IOTA', 'KAPPA', 'LAMBDA', 'MU', 'NU', 'XI', 'RHO', 'SIGMA', 'TAU', 'UPSILON', 'CHI', 'PSI', 'OMEGA', 'PI', 'ALPHA', 'BETA', 'PRIME', 'DPRIME', 'DOT', 'SUBSCRIPT', 'VEC', 'OVERLINE', 'ABS', 'FRAC_GROUP', 'PARALLELOGRAM'], defaultValue: "\\left(4\\right)^5\\sqrt5{9}" }), inputlist[index]);
-}
+// for(var index=0;index<inputlist.length;index++)
+// {
+// 	var myid = inputlist[index].id;
+// 	ReactDOM.render(<MathInput containerid={myid} focusCallback={()=>{
+//         console.log('on focus')
+//     }} blurCallback={() => {
+//         console.log('did dismiss')
+//     }} extraKeys={['CUP', 'CAP', 'INFTY', 'HALFCOORDINATE', 'HALFCLOSURE', 'COORDINATE', 'CLOSURE', 'DELTA', 'BIGTRIANGLEUP', 'HUO', 'SIM', 'CONG', 'PERP','QIE','APPROX', 'TRIANGLE', 'BECAUSE', 'THEREFORE', 'ODOT', 'MAO', 'ANGLE', 'JIAO', 'PERCENT', 'DU', 'DEGREE', 'GAMMA', 'EPSILON', 'VAREPSILON', 'ZETA','ETA','VARTHETA','IOTA','KAPPA', 'LAMBDA','MU','NU','XI','RHO','SIGMA','TAU', 'UPSILON', 'CHI', 'PSI', 'OMEGA','PI','ALPHA','BETA','PRIME','DPRIME','DOT','SUBSCRIPT','VEC','OVERLINE','ABS','FRAC_GROUP','PARALLELOGRAM']} defaultValue={"\\left(4\\right)^5\\sqrt5{9}"}/>, inputlist[index],);
+//   }
+
 
 module.exports = MathInput;
